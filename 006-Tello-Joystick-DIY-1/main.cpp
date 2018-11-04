ce7f6ceb34ac96ddca9c2c476272903fb3e048be
makepluscode <39910774+makepluscode@users.noreply.github.com>
2018-11-04 20:42:40 +0900

    Update main.cpp

--- 006-Tello-Joystick-DIY-1/main.cpp
+++ 006-Tello-Joystick-DIY-1/main.cpp
@@ -1,17 +1,14 @@
-#include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
 WiFiUDP udp;
 
-const char *SSID = "TELLO-C698AD";
-const char *PASS = "";
-const char *ADDR = "192.168.10.1";
-const int  PORT  = 8889;
+// Make sure to add your own information
+const char *SSID = "****";
+const char *PASS = "****";
 
 void setup()
-{
+{ 
   Serial.begin(74880);
 
   WiFi.begin(SSID, PASS);
@@ -21,59 +18,27 @@ void setup()
     delay(100);
     Serial.print(".");
   }
-  Serial.println("Connected!");
-
-  Serial.println("[NETWORK]");
+  Serial.print("Connected! ");
   Serial.println(WiFi.localIP());
-  Serial.println(WiFi.macAddress());
-  Serial.println("");
 
-  udp.begin(PORT);
-}
-
-void SendCmd(char *cmd)
-{
-  char buf[255];
-  Serial.print("Wemos sent : ");
-  Serial.println(cmd);
-  
-  udp.beginPacket(ADDR, PORT);
-  udp.printf(cmd);
-  udp.endPacket();
-
-  memset(buf, 0x0, sizeof(buf));
-
-  while(udp.parsePacket() <= 0)
-  {
-    delay(5);
-  }
-
-  if(udp.read(buf, sizeof(buf)) > 0)
-  {
-    Serial.print("Tell said : ");
-    Serial.println(buf);
-  }
+  udp.begin(1004);
 }
 
 void loop()
 {
-  SendCmd((char *)"command");
-  SendCmd((char *)"time?");
-  SendCmd((char *)"battery?");  
-  SendCmd((char *)"speed?");
-  SendCmd((char *)"speed 50");
-  SendCmd((char *)"speed?");  
-  
-  SendCmd((char *)"takeoff");
-  delay(5000);
-  SendCmd((char *)"land");
-  delay(5000);
+  char buffer[255];
+  int size = udp.parsePacket();
 
-  Serial.println("All done!");
-
-  while(true)
+  if (size)
   {
-    //do nothing
-    delay(1000);
+    int len;
+    memset(buffer, 0x0, sizeof(buffer));
+
+    len = udp.read(buffer, sizeof(buffer));
+    Serial.printf("Received Packet : %s\n", buffer);
+
+    udp.beginPacket(udp.remoteIP(), udp.remotePort());
+    udp.write("Welcomm to Wemos");
+    udp.endPacket();
   }
 }
